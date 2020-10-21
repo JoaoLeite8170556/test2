6b130aeaaa9901e0669c5174859bccb90cd4c403
João Carlos Carvalho Leite <joao-leite8@hotmail.com>
2020-10-21 16:58:20 +0100

    adicionei duas pastas

---
+++ Shell.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Shell{
+public static void main(String[] args) throws IOException {
+    //verifica se tem algum argumento 
+    if (args.length != 1) {
+        System.err.println("Modo de uso: java Shell <comando>"); 
+        System.exit(0);
+        }
+    // args[0] comando a executar
+    ProcessBuilder pb = new ProcessBuilder(args[0]);
+    Process process = pb.start();
+    System.out.println("Inicio do comando \""+args[0]+"\".");
+    try{
+        process.waitFor();
+        System.out.println("Fim do comando \""+args[0]+"\".");
+    }catch(InterruptedException e){}
+    }
+}
